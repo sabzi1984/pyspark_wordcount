13cb67e2d9df40a2b73cd38981955c1372af6f47
ALI Akbar SABZI <57127882+sabzi1984@users.noreply.github.com>
2021-11-02 10:44:27 -0400

    Add files via upload

--- spark_wordCount.py
+++ spark_wordCount.py
@@ -1,6 +1,3 @@
-# import os
-# os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
-
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark import SparkFiles
@@ -19,7 +16,7 @@ def reduce_words(value1, value2):
     return value1+value2
 
     
-exec_time_list=[]
+# exec_time_list=[]
 book_list=[]
 
 
@@ -30,39 +27,41 @@ urls=['http://www.gutenberg.ca/ebooks/buchanj-midwinter/buchanj-midwinter-00-t.t
 book_titles=['buchanj-midwinter-00-t.txt','carman-farhorizons-00-t.txt','colby-champlain-00-t.txt','cheyneyp-darkbahama-00-t.txt','delamare-bumps-00-t.txt',
             'charlesworth-scene-00-t.txt','delamare-lucy-00-t.txt','delamare-myfanwy-00-t.txt','delamare-penny-00-t.txt']
 spark = SparkSession.builder.getOrCreate()
-start_add_file_time = time.time()
+# start_add_file_time = time.time()
 for j in range(len(urls)):
   spark.sparkContext.addFile(urls[j])
 end_add_file_time = time.time()
 
-start_data_read = time.time()
+# start_data_read = time.time()
 for i in range(len(book_titles)):
 
   book_list.append(spark.read.csv("file://"+SparkFiles.get(book_titles[i]), header=False, inferSchema= True) )
 end_read_time = time.time()
-for _ in range(3):
-  start = time.time()
-  for i in range(len(book_titles)):
 
-    # book = spark.read.csv("file://"+SparkFiles.get(book_titles[i]), header=False, inferSchema= True)
-    # print(book1_rdd.collect())
-    book_rdd = book_list[i].rdd
+# start = time.time()
+for i in range(len(book_titles)):
 
-    book_rdd = book_rdd.flatMap(map_words)
-    # print(book1_rdd.collect())
+  # book = spark.read.csv("file://"+SparkFiles.get(book_titles[i]), header=False, inferSchema= True)
+  # print(book1_rdd.collect())
+  book_rdd = book_list[i].rdd
 
-    book_rdd = book_rdd.reduceByKey(reduce_words)
-    # print(book1_rdd.collect())
+  book_rdd = book_rdd.flatMap(map_words)
+  # print(book1_rdd.collect())
 
-    # book_rdd.toDF(["word", "count_word"]).show()
+  book_rdd = book_rdd.reduceByKey(reduce_words)
+  # print(book1_rdd.collect())
+
+  # book_rdd.toDF(["word", "count_word"]).show()
+  yield(book_rdd.toDF(["word", "count_word"]))
 
 
-  end = time.time()
-  exec_time=end-start
-  exec_time_list.append(exec_time)
-reading_data_time=end_read_time-start_data_read
-adding_data_time=end_add_file_time-start_add_file_time
 
-print(f'MapReduce Time:{exec_time_list}')
-print(f'Reading Data Time :{reading_data_time}')
-print(f'add Data Time :{adding_data_time}')
+#   end = time.time()
+#   exec_time=end-start
+#   exec_time_list.append(exec_time)
+# reading_data_time=end_read_time-start_data_read
+# adding_data_time=end_add_file_time-start_add_file_time
+
+# print(f'MapReduce Time:{exec_time_list}')
+# print(f'Reading Data Time :{reading_data_time}')
+# print(f'add Data Time :{adding_data_time}')
